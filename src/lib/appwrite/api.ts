7213815c6fbaedb0678daa8a26b6194a281e0553
25LUKE <ericluck25.com>
2023-11-26 18:38:14 +0100

    media

---
+++ src/lib/appwrite/api.ts
@@ -0,0 +1,97 @@
+import { INewUser } from "@/type";
+import { ID, Query } from 'appwrite';
+import { account, appwriteConfig, avatars, databases } from "./config";
+
+export async function createUserAccount(user: INewUser) {
+    try {
+        const newAccount = await account.create(
+            ID.unique(),
+            user.email,
+            user.password,
+            user.name,
+        );
+
+        if(!newAccount) throw Error;
+
+        const avatarsUrl = avatars.getInitials(user.name);
+
+        const newUser  = await saveUserToDB({
+            accountId: newAccount.$id,
+            name: newAccount.name,
+            email: newAccount.email,
+            username: user.username,
+            imageUrl: avatarsUrl,
+
+        })
+
+        return newUser
+    } catch (error) {
+        console.log(error)
+        return error;
+    }
+}
+
+export async function saveUserToDB(user: {
+    accountId: string;
+    email: string;
+    name: string;
+    imageUrl: URL;
+    username: string;
+}) {
+    // Lets save a duc to appwrite database
+    try {
+        const newUser = await databases.createDocument(
+            appwriteConfig.databaseId, //We need to know which database are we modifying
+            appwriteConfig.usercollectionId,
+            ID.unique(),
+            user,
+        )
+
+        return newUser
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function signInAccount(user: {
+    email: string;
+    password: string;
+}) {
+   try {
+    const session = await account.createEmailSession(user.email, user.password);
+
+    return session;
+   } catch (error) {
+    console.log(error)
+   } 
+}
+
+export async function getCurrentUser() {
+    try {
+        const currentAccount = await account.get();
+
+        if(!currentAccount) throw Error;
+
+        const currentUser = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.usercollectionId,
+            [Query.equal('accountId', currentAccount.$id)]
+        )
+
+        //if(!currentAccount) throw Error;
+
+        return currentUser.documents[0]
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+export async function signOutAccount() {
+    try{
+        const session = await account.deleteSession("current");
+
+        return session;
+    } catch (error) {
+        console.log(error)
+    }
+}
